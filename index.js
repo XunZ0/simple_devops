754256c38a9272e57a1500bd6b0685c8f66dd5d1
XunZ0 <113183729+XunZ0@users.noreply.github.com>
2022-09-09 20:47:19 +0800

    Hello World

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Simple DevOps')
+  res.send('Hello World')
 })
 
 app.listen(process.env.PORT, ()=>{
